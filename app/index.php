a0151cfb3a68eca1f9bccb5518ce27389957679e
Binod Chaudhary <thebinod7@gmail.com>
2025-02-03 16:54:45 +0545

    added hot reload

--- app/index.php
+++ app/index.php
@@ -1,18 +1,15 @@
-<?php
-$servername = "mysql";  // Service name defined in docker-compose.yml
-$username = "root";
-$password = "root";
-$database = "mydb";
 
-try {
-    // Create connection
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
 
-    // Set PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    echo "Connected to MySQL successfully!";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>
+        <?php echo "Hello Worldy!" ?>
+    </h3>
+</body>
+</html>
